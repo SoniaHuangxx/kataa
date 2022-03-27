7c4e8ac1bf2dca629f32dd9fb66ad442f7e0256e
Sonia Huang <sonia.huang@titansoft.com.sg>
2022-03-27 23:54:23 +0800

    refactor

--- WeighForWeight/WeighForWeight/WeightSort.cs
+++ WeighForWeight/WeighForWeight/WeightSort.cs
@@ -5,11 +5,9 @@ public class WeightSort
     //https://www.codewars.com/kata/55c6126177c9441a570000cc
     internal string WeightSorter(string strng)
     {
-        var split = strng.Trim().Split(" ").ToList();
-
-        return string.Join(" ", split
-            .OrderBy(s => s
-                .Select(x => Convert.ToInt32(x.ToString())).Sum())
+        return string.Join(" ", strng.Trim().Split(" ")
+            .OrderBy(x => x
+                .Select(y => Convert.ToInt32(y.ToString())).Sum())
             .ThenBy(x => x));
     }
 }
